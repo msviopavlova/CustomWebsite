664611e49f25a4b93e869d001a0f326081284846
Violetta Pavlova <msviopavlova@gmail.com>
2021-01-22 19:12:20 +0000

    navbar back

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@ var topbutton = document.getElementById("myBtn")
 onscroll = function () {ifScrolledFunction()}
 
 function ifScrolledFunction() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         topbutton.style.display = "block";
       } else {
         topbutton.style.display = "none";
